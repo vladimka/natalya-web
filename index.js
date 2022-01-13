423ef6d82e5f5f0d274325b4b525f72a5a4acbbb
vladimka <zverevv.04@gmail.com>
2022-01-13 15:34:48 +0800

    фикус

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const expresss = require("express");
+const express = require("express");
 const morgan = require('morgan');
-const app = expresss();
+const app = express();
 
 app.use(morgan('dev'));
-app.use(expresss.static("./public"));
+app.use(express.static("./public"));
+
+app.post('/query', express.json(), async (req, res) => {
+    console.log(req.body);
+    return await res.json({ answer : new Date(Date.now()).toLocaleDateString() });
+});
 
 app.listen(80, "0.0.0.0", () => console.log("Server started"));
--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ const queryForm = document.querySelector('.query-form');
 const historyUl = document.querySelector('.history');
 const queryInput = document.querySelector('.query-input');
 
-queryForm.onsubmit = e => {
+queryForm.onsubmit = async e => {
     e.preventDefault();
 
     let text = queryInput.value.trim();
@@ -11,17 +11,27 @@ queryForm.onsubmit = e => {
         return;
 
     historyUl.innerHTML += `
-        <li class="user-text shadow">${text}</li>
+        <div class="user-text shadow">${text}</div>
     `;
 
-    let answer = getAnswer(text);
+    let answer = await getAnswer(text);
     historyUl.innerHTML += `
-        <li class="natalya-answer shadow">${answer}</li>
+        <div class="natalya-answer shadow">${answer}</div>
     `;
 
     queryInput.value = '';
 }
 
-function getAnswer(text){
-    return "Извините, я, пока что, вас не понимаю:(";
+async function getAnswer(text){
+    const data = await fetch('/query', {
+            method : 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({ query : text })
+        })
+        .then(res => res.json())
+        .catch(console.error);
+    console.log(data);
+    return data.answer;
 }
